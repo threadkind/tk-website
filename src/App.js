836bb787c763fba9c75d622aac992ba2353736d7
threadkind <threadkind@gmail.com>
2018-11-25 11:51:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import banner from './images/threadkind-banner.jpeg';
 import './App.css';
 
 class App extends Component {
@@ -7,10 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <img src={banner} className="header-banner" alt="threadkind banner" />
+
           <a
             className="App-link"
             href="https://reactjs.org"
